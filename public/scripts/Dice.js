0596420d836982521441cea82cc99d5a9b789979
Daniel McRae <dtmcrae0@gmail.com>
2023-05-11 09:54:54 -0700

    Added static functions to be called in console for debugging.

--- public/scripts/Dice.js
+++ public/scripts/Dice.js
@@ -105,3 +105,79 @@ async function VisualizeRoll(faces, amount)
         return roll;
     }, 500);
 }
+
+class Dice {
+
+    static Roll(faces, amount)
+    {
+        const validFaces = [4,6,8,10,12,20,100];
+
+        if(!validFaces.includes(faces))
+        {
+            console.error("Invalid face count for attempted dice roll. \nGiven Face: " + faces + "\nValid Faces: ",validFaces);
+            return -1;
+        }
+
+        var roll = 0;
+
+        for(var i = 0; i < amount; i++)
+        {
+            roll += Math.ceil(Math.random() * faces);
+        }
+        console.log("Rolled a d" + faces + ": " + roll);
+        return roll;
+    }
+
+    static VisualizeRoll(faces, amount)
+    {
+
+        const changeDelay = 100; //0.2 Second
+
+    //How many times the value will change;
+        const totalLength = 20;
+    if (!validFaces.includes(faces)) {
+        console.error("Invalid face count for attempted dice roll. \nGiven Face: " + faces + "\nValid Faces: ", validFaces);
+        return -1;
+    }
+
+    //An internal function for use in VisualizeRoll only.
+    function displayVariance(elementID, faces, result) {
+        let element = $(`#${elementID}`);
+
+        var inc = 0;
+
+        function loopChange() {
+            var value = Math.ceil(Math.random() * faces);
+            element.html(value);
+            if (++inc < totalLength) setTimeout(loopChange, changeDelay);
+        }
+
+        loopChange();
+
+        element.html(result);
+    }
+
+    var roll = 0;
+
+    if(document.getElementById("diceModal") == null)
+    {
+        $('body').append(`<div id="diceModalContainer"></div>`);
+        $('#diceModalContainer').load("views/templates/diceModal.ejs");
+    }
+
+    setTimeout(function() {
+        $('.modal-body').empty();
+        $('#diceModal').modal('show');
+
+    for (var i = 0; i < amount; i++) {
+        var value = Math.ceil(Math.random() * faces);
+        roll += value;
+        
+        $('.modal-body').append(`<div id="dice${i}"></div>`);
+        displayVariance(`dice` + i, faces, roll);
+    }
+        return roll;
+    }, 500);
+}
+
+}
