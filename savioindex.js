10bbc8dec13a40bd6f0b0eb2b1f877bd6a1fcdf0
sdsouza42 <dsouza.savio@hotmail.com>
2023-05-18 02:17:14 -0700

    Updates for sprint 4, backend function, level up page, other change made directly to database (adding more data & creating collections)

--- savioindex.js
+++ savioindex.js
@@ -33,6 +33,7 @@ var {database} = require('./databaseConnection');
 const equipCollection = database.db(mongodb_database).collection('EQUIPMENT');
 const charCollection = database.db(mongodb_database).collection('USERCHAR');
 const savedCollection = database.db(mongodb_database).collection('USERSAVED');
+const monstersCollection = database.db(mongodb_database).collection('MONSTERS');
 
 //This constant will save the objectid
 const { ObjectId } = require('mongodb'); 
@@ -75,6 +76,99 @@ const equipmentList = await equipCollection.find().toArray();
 return equipmentList.map((equip) => equip.Name).join(', ');
 }
 
+//function for loading all the monster names into an array
+async function getMonsterNames() {
+const monstersList = await monstersCollection.find({}, { projection: { _id: 0, name: 1 } }).toArray();
+return monsterList = monsters.map(monster => monster.name);
+}
+
+//fucntion for return an object containing spefic Monster information
+async function getMonsterInfo(name) {
+const monsterInfo = await monstersCollection.findOne({ name: name });
+return monsterInfo;
+}
+
+//function for defing ac
+async function calculateAC(character) {
+  let baseAC = 10; // base AC when no armor is worn
+  let dexMod = Math.floor((character.AbilityScores.Dexterity - 10) / 2); // calculate Dexterity modifier
+
+  // the AC values and max DEX mod bonus are predefined for simplicity. Ideally, these should be in a database or another data structure
+  const armorValues = {
+      'Leather': [11, null],
+      'Studded leather': [12, null],
+      'Hide': [12, 2],
+      'Chain shirt': [13, 2],
+      'Scale mail': [14, 2],
+      'Breastplate': [14, 2],
+      'Half plate': [15, 2],
+      'Ring mail': [14, null],
+      'Chain mail': [16, null],
+      'Splint': [17, null],
+      'Plate': [18, null],
+  };
+
+  let armor = character.Equipment.Armour;
+  let shield = character.Equipment.Additional_Equipment_3 === 'Shield';
+  let charClass = character.Class;
+  let unarmoredDefense = character.Abilities.includes('Unarmored Defense');
+
+  if (armor in armorValues) {
+      let [baseACArmor, maxDexBonus] = armorValues[armor];
+      if (maxDexBonus !== null) {
+          dexMod = Math.min(dexMod, maxDexBonus); // limit DEX mod bonus if there is a maximum
+      }
+      baseAC = baseACArmor;
+  } else if (unarmoredDefense && armor === 'None') {
+      if (charClass === 'Barbarian') {
+          let conMod = Math.floor((character.AbilityScores.Constitution - 10) / 2); // calculate Constitution modifier
+          baseAC += conMod;
+      } else if (charClass === 'Monk') {
+          let wisMod = Math.floor((character.AbilityScores.Wisdom - 10) / 2); // calculate Wisdom modifier
+          baseAC += wisMod;
+      }
+  }
+
+  let ac = baseAC + dexMod;
+  if (shield) {
+      ac += 2; // add 2 for shield
+  }
+
+  return ac;
+}
+
+async function getLevelUpData(userClass, userLevel) {
+  try {
+      const levelData = await database.collection('LEVEL').findOne({ class: userClass });
+      return levelData;
+  } catch (err) {
+      console.error(err);
+      return null;
+  }
+}
+
+async function getSpellData(userClass, userLevel) {
+  try {
+      const spellData = await database.collection('SPELLS').find({ classes: new RegExp(userClass, 'i'), level: { $lte: userLevel } }).toArray();
+      return spellData;
+  } catch (err) {
+      console.error(err);
+      return null;
+  }
+}
+
+async function spellcasting(userClass) {
+  try {
+      // For Wizards and Druids at level 1, we will only fetch level 0 (cantrips) and level 1 spells.
+      const spellData = await database.collection('SPELLS').find({ classes: new RegExp(userClass, 'i'), level: { $in: [0, 1] } }).toArray();
+      return spellData;
+  } catch (err) {
+      console.error(err);
+      return null;
+  }
+}
+
+
 /* Reference for outer joints
 breakdown of the $lookup stage:
 from: Specifies the collection to join with the input documents (in this case, the EQUIPMENT collection).
@@ -85,8 +179,6 @@ foreignField: Specifies the field from the documents of the from collection (equ
 as: Specifies the name of the new array field that will be added to the input documents. This new field contains the matching documents from the from collection.
      In this case, the field is named inventoryDetails.
 */
-
-
 
 async function getPlayerInventory(characterId) {
     const result = await charCollection.aggregate([
@@ -199,4 +291,46 @@ app.get('/equipped', fetchPlayerEquippedItemsMiddleware, (req, res) => {
 //Remove once app completed
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
+});
+
+//level up page
+// app.get('/levelup', async function(req, res) {
+//   const userId = req.session.userId; // replace this with however you're identifying your user
+
+//   if (!userId) {
+//       // handle the case where there is no logged in user
+//       return res.status(401).send('You must be logged in to view this page.');
+//   }
+
+//   const characterData = await getCharacterData(userId); // function that fetches character data from the database
+//   const skillOptions = await getSkillOptions(characterData.class, characterData.currentLevel + 1); // function that fetches skill options
+//   const subclassOptions = characterData.currentLevel === 1 ? await getSubclassOptions(characterData.class) : [];
+
+//   res.render('levelup', {
+//       characterData: characterData,
+//       skillOptions: skillOptions,
+//       subclassOptions: subclassOptions
+//   });
+// });
+
+//test level up page
+app.get('/levelup', async (req, res) => {
+  const testCharacterData = {
+      name: "Test Wizard",
+      currentLevel: 1,
+      class: "Wizard",
+      subclass: "",
+      nextLevel: 2
+  };
+
+  const levelUpData = await getLevelUpData(testCharacterData.class, testCharacterData.nextLevel);
+  const spellData = await spellcasting(testCharacterData.class);
+  
+  const fullCharacterData = {
+      ...testCharacterData,
+      ...levelUpData,
+      spells: spellData
+  };
+
+  res.render('character', fullCharacterData);
 });
