26c703bc98de9cb0bb688ab2297011c863dfd0f5
Justin Saint <justinsaintdev@gmail.com>
2023-05-20 01:21:09 -0700

    Completed and debugged introduction scene and NPC dialogue scene with four pages total (initial scene and three interactive questions)

--- routes/story.js
+++ routes/story.js
@@ -11,13 +11,14 @@ const {
 const mongodb_database = process.env.MONGODB_DATABASE;
 
 // Arrays for story generation. (note: N is for "Normal" games, B is for BCIT "Easter egg games")
-const typesN = ['', '', '', '', '', '', '', '', 'about a lost villager', 'about fighting bandit raiders', 'about fighting a gang of thugs', 'about a bounty hunt', 'about fighting goblins', 'about a magic portal', 'about defending a village', 'about stealing back an artifact', 'about a haunted mansion', 'about a separated couple', 'about a missing royal', 'about a rescue', 'about a heist', 'about a rivalry', 'about a journey', 'about an intruder', 'about a rebellion', 'about an artifact', 'about a prophecy', 'about a tournament', 'about an escape', 'about a hunt', 'about a treasure hunt', 'about strange magic', 'about a tower defense', 'about a lost cat', 'about BCIT'];
+const typesN = ['', '', '', '', 'about a lost villager', 'about fighting bandit raiders', 'about fighting a gang of thugs', 'about a bounty hunt', 'about fighting goblins', 'about a magic portal', 'about defending a village', 'about stealing back an artifact', 'about a haunted mansion', 'about a separated couple', 'about a missing royal', 'about a rescue', 'about a heist', 'about a rivalry', 'about a journey', 'about an intruder', 'about a rebellion', 'about an artifact', 'about a prophecy', 'about a tournament', 'about an escape', 'about a hunt', 'about a treasure hunt', 'about strange magic', 'about a tower defense', 'about a lost cat',];
 const typesB = ['', 'about time travel back to BCIT opening in 1969', 'about squeaky classroom chairs', 'about a campus coffee shortage', 'about an extremely long line at Tim Horton\'s', 'about loud noises at the BCIT library', 'about an overcrowded BCIT gym', 'about find a parking spot', 'about a lost student', 'about fighting a rival school', 'about fighting school bullies', 'about missing textbooks', 'about a cursed exam', 'about a magic portal', 'about defending the campus', 'about stealing back stolen documents', 'about a haunted building', 'about separated friends', 'about a team of students working on an AI project', 'about an Easter Egg mission', 'about AI', 'about a student imposter', 'about a rescue', 'about restoring power to campus', 'about cursed code', 'about rogue AI', 'about escaping a virtual reality program', 'about a cybersecurity breach', 'about a vanishing classroom', 'about a lost cat'];
 const bcitN = '';
 const bcitB = 'at BCIT';
 
 // Array around the main character's relationship with the starting location
 const random_class = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'warlock', 'wizard', 'BCIT student'];
+const emotion_NPC = ['desperate', 'hopeless', 'fearful', 'anxious', 'weary', 'skeptical', 'grateful', 'resigned', 'suspicious', 'regretful', 'grieiving', 'nervous', 'awestruck', 'excited', 'curious', 'hopeful', 'relieved', 'happy'];
 const verb_location = ['living in', 'arriving at', 'visiting', 'exploring', 'investigating', 'found themselves in', 'randomly found', 'lived all their life in'];
 
 // Test values for story generation
@@ -37,29 +38,52 @@ const generateStoryPrompt = (randomType, bcit) => {
 
     return `Imagine you are creating a detailed DnD adventure ${randomType} ${bcit}. Please provide the following details:
 
-- "Title": The title of the adventure.
-- "Summary": An overall summary of the adventure that describes the central conflict or goal. Please write the summary in three sentences. Leave the outcome a mystery to maintain the suspense.
-- "Goal": Describe the central goal of the adventure in one sentence. Use the format "<action> the <noun>"
-- "S_Start": The name of the location where the story starts. Only provide the name.
-- "S_Boss": The location where the central conflict or goal can be resolved. Only provide the name.
- 
-
-Please structure your response in the following format:
-
-{
-  "title": "<title>",
-  "summary": "<summary>",
-  "goal": "<goal>",
-  "s_start": "<setting>"
-  "s_boss": "<setting>"
-
-}`;
+    - "title": The title of the adventure.
+    - "summary": An overall summary of the adventure that describes the central conflict or goal. Please write the summary in three sentences. Leave the outcome a mystery to maintain the suspense.
+    - "goal": Describe the central goal of the adventure in one sentence. Use the format "<action> the <noun>"
+    - "s_start": The name of the location where the story starts. Only provide the name.
+    - "s_soss": The location where the central conflict or goal can be resolved. Only provide the name.
+    - "npc_role": The role of the NPC who tells you about the adventure. Only provide their title.
+     
+    
+    Please structure your response in the following format:
+    
+    {
+      "title": "<title>",
+      "summary": "<summary>",
+      "goal": "<goal>",
+      "s_start": "<setting>"
+      "s_boss": "<setting>"
+      "npc_role": "<role>"
+    
+    }`;
 };
 
-const generateIntro = (selectedClass, verb, s_start) => {
-    
-    return `Start a DnD adventure about a player character of the "${selectedClass}" class who is "${verb}" "${s_start}" in four sentences. Describe the character and the setting. Don't introduce any conflict. `;
+const generateIntro = (selectedClass, verb, s_start, NPC, npc_role) => {    
+    return `Start a DnD adventure about a player character of the "${selectedClass}" class who is "${verb}" "${s_start}" in four sentences. Refer to the character as "you" after introducing them. Describe the character and the setting. End the scene by having ${NPC} the ${npc_role} call out to the main character. Don't introduce any conflict. `;
+};
 
+const generateNPC = (NPC, emotion, characters, goal) => {
+    return `A "${emotion}" "${NPC}" tells ${characters[0].name} that they must ${goal}. Remember that they are already talking. Describe the scene in two sentences.`;
+};
+
+const generateDialogue = (qtopic, NPC, characters) => {
+    return `${characters[0].name} asks ${NPC} ${qtopic}. Write one sentence per dialogue line.
+
+    - "Q1": ${characters[0].name}'s first question to ${NPC}
+    - "A1": ${NPC}'s response
+    - "Q2": A follow-up question
+    - "A2": Answer to the follow-up
+         
+    Please structure your response in the following format:
+    
+    {
+      "Q1": "${characters[0].name}: <question>",
+      "A1": "${NPC}: <answer>",
+      "Q2": "${characters[0].name}: <question>",
+      "A2": "${NPC}: <answer>"
+    
+    }`;
 };
 
 const generateEventPrompt = (eventNumber, events, NPC, goal, s_start, s_boss, characters, enemies, boss) => {
@@ -153,10 +177,8 @@ router.post('/generateStory', async (req, res) => {
         const randomType = getRandomElement(types);
         console.log('Story type: ', randomType);
 
-        // Creates the story
+        // Creates the story and parses the text into a JSON object
         const responseText = await openAI.generateText(generateStoryPrompt(randomType, bcit), model, 1600);
-
-        // Parse the text into a JSON object
         const responseObject = JSON.parse(responseText);
 
         // Store the generated story summary and events in the session
@@ -166,21 +188,24 @@ router.post('/generateStory', async (req, res) => {
         req.session.goal = responseObject.goal;
         req.session.s_start = responseObject.s_start;
         req.session.s_boss = responseObject.s_boss;
+        req.session.npc_role = responseObject.npc_role;
 
+        req.session.currentEvent = 1;
         req.session.events = {
-            "1": { "type": "story_intro", "json": false },
-            "2": { "type": "story_NPC", "json": true },
-            "3": { "type": "story_scene", "json": false },
-            "4": { "type": "NPC_Q2", "json": false },
-            "5": { "type": "SkillCheck_prompt", "json": true },
-            "6": { "type": "SkillCheck_fail", "json": false },
-            "7": { "type": "SkillCheck_partial", "json": false },
-            "8": { "type": "SkillCheck_success", "json": false },
-            "9": { "type": "SkillCheck_full", "json": false },
-            "10": { "type": "Battle", "json": false },
-            "11": { "type": "Story_Event", "json": false },
-            "12": { "type": "Boss", "json": false },
+            "1": { "type": "story-intro" },
+            "2": { "type": "story-npc" },
+            "3": { "type": "story-scene" },
+            "4": { "type": "NPC_Q2" },
+            "5": { "type": "SkillCheck_prompt" },
+            "6": { "type": "SkillCheck_fail" },
+            "7": { "type": "SkillCheck_partial" },
+            "8": { "type": "SkillCheck_success" },
+            "9": { "type": "SkillCheck_full" },
+            "10": { "type": "Battle" },
+            "11": { "type": "Story_Event" },
+            "12": { "type": "Boss" },
         };
+       
 
         console.log('Character Class:', req.session.selectedClass);
         console.log('Summary:', req.session.summary);
@@ -188,6 +213,7 @@ router.post('/generateStory', async (req, res) => {
         console.log('Goal:', req.session.goal);
         console.log('Start location:', req.session.s_start);
         console.log('Boss location:', req.session.s_boss);
+        console.log('NPC role:', req.session.npc_role);
 
         // Sends title and summary to the story generation screen
         res.render('story', {
@@ -203,18 +229,15 @@ router.post('/generateStory', async (req, res) => {
     }
 });
 
-
-
 // Handles GET requests and moves player to the next event
 router.get('/story-event', async (req, res) => {
 
     const event = getNextEvent(req);
     
-
     // Generates a different event page depending on the next event type
     switch (event.type) {
 
-        case 'story_intro':
+        case 'story-intro':
 
             const randomVerb = getRandomElement(verb_location);
 
@@ -224,14 +247,24 @@ router.get('/story-event', async (req, res) => {
                 console.log('Random class: ', randomClass);
             }
 
-            const responseText = await openAI.generateText(generateIntro(req.session.selectedClass, randomVerb, req.session.s_start), model, 800);
+            const introText = await openAI.generateText(generateIntro(req.session.selectedClass, randomVerb, req.session.s_start, NPC, req.session.npc_role), model, 800);
 
-            res.render('story-intro', { text: responseText });
+            res.render('story-intro', { text: introText  });
             break;
 
-        case 'story_NPC':
-            // Handle story_NPC event here...
+        case 'story-npc':
+
+            const randomEmotion = getRandomElement(emotion_NPC);
+
+            const npcText = await openAI.generateText(generateNPC(NPC, randomEmotion, characters, req.session.goal), model, 800);
+
+            // Generates an array of questions to ask the NPC
+            req.session.questions = ['mission', 'npc', 'setting'];
+
+            res.render('story-npc', { text: npcText, emotion: randomEmotion, goal: req.session.goal, NPC: NPC, s_start: req.session.s_start, questions: req.session.questions, npc_role: req.session.npc_role});
+
             break;
+
         case 'story_scene':
             // Handle story_scene event here...
             break;
@@ -244,5 +277,49 @@ router.get('/story-event', async (req, res) => {
     }
 });
 
+// Creates dialogue with NPC based on the selected buttons
+router.get('/npc-dialogue', async (req, res) => {
+
+    const questionKey = req.query.question;
+    let qtopic;
+    
+    // Removes a question if it was already asked
+    const questionIndex = req.session.questions.indexOf(questionKey);
+    if (questionIndex > -1) {
+      req.session.questions.splice(questionIndex, 1);
+    }
+
+    // Changes the prompts based on the button pressed
+    switch (questionKey) {
+        case 'mission':
+            qtopic = 'why ' + NPC + needs a ' + req.session.selectedClass + ' to ' + req.session.goal;
+            break;
+
+        case 'npc':
+            qtopic = 'more about their role as ' + req.session.npc_role;
+            break;
+
+        case 'setting':
+            qtopic = 'about ' + req.session.s_start;
+            break;
+    }
+
+    console.log('Question Key:', questionKey);
+    console.log('Session Questions:', req.session.questions);
+    console.log('Question Index:', questionIndex);
+    console.log('QTopic:', qtopic);
+
+    const dialogueText = await openAI.generateText(generateDialogue(qtopic, NPC, characters), model, 800);
+    console.log('Response from OpenAI: ', dialogueText);
+    const dialogueObject = JSON.parse(dialogueText);
+
+    req.session.Q1 = dialogueObject.Q1;
+    req.session.A1 = dialogueObject.A1;
+    req.session.Q2 = dialogueObject.Q2;
+    req.session.A2 = dialogueObject.A2;
+
+    res.render('npc-dialogue', {Q1: req.session.Q1, A1: req.session.A1, Q2: req.session.Q2, A2: req.session.A2, goal: req.session.goal, NPC: NPC, s_start: req.session.s_start, questions: req.session.questions});
+    
+});
 
 module.exports = router;
