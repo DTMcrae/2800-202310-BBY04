9eb26d7a0a7743653d4e8e4c8bb0ca5784698e22
Daniel McRae <dtmcrae0@gmail.com>
2023-05-21 18:53:39 -0700

    Added a function to re-attempt chatGPT prompts should a server error occur.

--- public/scripts/combatAI.js
+++ public/scripts/combatAI.js
@@ -13,6 +13,7 @@ class CombatAI {
 
     //Custom request for getting what dice need to be rolled from chatGPT.
     async generateResponse(systemPrompt, userPrompt, tokens, temp) {
+        this.breakout = 0;
         try {
             // Send a request to the OpenAI API to generate text
             const response = await this.openai.createChatCompletion({
@@ -27,18 +28,50 @@ class CombatAI {
                 max_tokens: tokens,
                 temperature: temp,
             });
-            console.log(`request cost: ${response.data.usage.total_tokens} tokens`);
             // Return the text of the response
             return response.data.choices[0].message.content;
         } catch (error) {
-            throw error;
+            const response = await this.#retryResponse(systemPrompt, userPrompt, tokens, temp);
+            return response.data.choices[0].message.content;
+        }
+    }
+
+    async #retryResponse(systemPrompt, userPrompt, tokens, temp) {
+        this.breakout++;
+
+        if(this.breakout >= 3) {
+            return null;
+        }
+
+        const waitFor = delay => new Promise(resolve => setTimeout(resolve, delay));
+        await waitFor(500);
+        
+        try {
+            // Send a request to the OpenAI API to generate text
+            const response = await this.openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [{
+                    role: "system",
+                    content: systemPrompt
+                }, {
+                    role: "user",
+                    content: userPrompt
+                }],
+                max_tokens: tokens,
+                temperature: temp,
+            });
+            // Return the text of the response
+            return response;
+        } catch (error) {
+            const response = await this.#retryResponse(systemPrompt, userPrompt, tokens, temp);
+            return response;
         }
     }
 
     //Custom request for getting what dice need to be rolled from chatGPT.
     async generateRollRequest(action) {
+        this.breakout = 0;
         try {
-            console.log("Roll Request Action:",action);
             // Send a request to the OpenAI API to generate text
             const response = await this.openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
@@ -57,12 +90,48 @@ class CombatAI {
                 max_tokens: 200,
                 temperature: 0.5,
             });
-            console.log(`Dice request cost: ${response.data.usage.total_tokens} tokens`);
-            console.log(`Response:`, response.data.choices[0].message.content);
             // Return the text of the response
             return response.data.choices[0].message.content;
         } catch (error) {
-            throw error;
+            const response = await this.#retryRollRequest(action);
+            return response.data.choices[0].message.content;
+        }
+    }
+
+    async #retryRollRequest(action) {
+        this.breakout++;
+
+        if (this.breakout >= 3) {
+            return null;
+        }
+
+        const waitFor = delay => new Promise(resolve => setTimeout(resolve, delay));
+        await waitFor(500);
+
+        try {
+            // Send a request to the OpenAI API to generate text
+            const response = await this.openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [{
+                    role: "system",
+                    content: "You are the dungeon master in a D&D 5E game, telling your players what dice they need to roll. Replace DamageDice and Stat with the dice needed to perform the action the player describes. DamageDice should only contain the type of dice and amount to roll in this format:`1d4`. Stat should be formatted like this:`Dexterity`. All communication should be in JSON format, no sentences. Leave any comments in a separate JSON field.",
+                },
+                {
+                    role: "user",
+                    content: `{
+                "Action": "${action}",
+                "DamageDice": "Unknown",
+                "Stat": "Unknown",
+                }`
+                }],
+                max_tokens: 200,
+                temperature: 0.5,
+            });
+            // Return the text of the response
+            return response;
+        } catch (error) {
+            const response = await this.#retryRollRequest(action);
+            return response;
         }
     }
 }
