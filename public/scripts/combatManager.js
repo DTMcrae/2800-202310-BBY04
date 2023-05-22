4cdfc7a495c5870393b3bd5b24f95537079a34ca
Daniel McRae <dtmcrae0@gmail.com>
2023-05-21 18:54:49 -0700

    Merged damage parses into a single function, and added a function to verify if chatGPT responses are valid.

--- public/scripts/combatManager.js
+++ public/scripts/combatManager.js
@@ -6,12 +6,9 @@ const CombatPrompts = require('./combatPrompts.js');
 const Dice = require('./Dice.js');
 
 const openAI = new CombatAI(process.env.OPENAI_KEY);
-const model = 'gpt-3.5-turbo';
-
-const mainPage = 'combat';
 
 //Preset enemy and player information for testing.
-function presetPlayers() { return [{ name: "Draeven", class: "Ranger", maxHP: 10, hp: 10, ac: 12, gold: 0, actions: [{ name: "Greatbow" }, { name: "Dagger" }, { name: "Axe" }, { name: "Quarterstaff" }] }, { name: "Asha", class: "Rogue", maxHP: 12, hp: 12, ac: 11, actions: [{ name: "Dual Daggers" }, { name: "Crossbow" }, { name: "Sneak Attack" }]}] }
+function presetPlayers() { return [{ name: "Draeven", class: "Ranger", maxHP: 10, hp: 99, ac: 12, gold: 0, actions: [{ name: "Greatbow" }, { name: "Dagger" }, { name: "Axe" }, { name: "Quarterstaff" }] }] }
 function presetEnemies() { return [{ name: "Goblin 1", hp: 7, ac: 9, desc: "A goblin wielding a dagger." }, { name: "Goblin 2", hp: 7, ac: 9, desc: "A goblin wielding a bow." }] }
 var players = [];
 var enemies = [];
@@ -20,48 +17,59 @@ const prompts = new CombatPrompts();
 var initiative = new TurnOrder();
 
 router.get('/', (req, res) => {
-    if(!req.session.authenticated)
-    {
-        res.redirect("/userLoginScreen");
-        return;
-    }
-    
-    // Initialize combat history
+    // if(!req.session.authenticated)
+    // {
+    //     res.redirect("/userLoginScreen");
+    //     return;
+    // }
+
+    initializeCombat(req, res);
+    req.session.lastURL = req.url;
+
+    var currentActor = initiative.currentTurn();
+    res.render('combat', { players: players, isPlayer: players.includes(currentActor), actor: currentActor, combat: combatStatus() });
+});
+
+router.post('/', (req, res) => {
+    var currentActor = initiative.currentTurn();
+    res.render('combat', { players: players, isPlayer: players.includes(currentActor), actor: currentActor, combat: combatStatus(), error: true });
+});
+
+//Placeholder function for assigning actors to combat.
+function initializeCombat(req, res) {
     req.session.history = [];
     var actors = [];
-    
+
     //Assign the preset players/enemies
     players = presetPlayers();
     enemies = presetEnemies();
-    
+
     //Roll initiative for each player
     players.forEach(player => {
-        player.roll = Dice.Roll(20,1);
+        player.roll = Dice.Roll(20, 1);
         player.isActive = (player.hp > 0);
         actors.push(player);
     })
 
     //Roll initiative for each enemy
     enemies.forEach(enemy => {
-        enemy.roll = Dice.Roll(20,1);
+        enemy.roll = Dice.Roll(20, 1);
         enemy.isActive = (enemy.hp > 0);
         actors.push(enemy);
     })
 
-    //Add every actor to the initative list, and get the first actor.
     initiative.assignNew(actors);
-    var currentActor = initiative.currentTurn();
     this.combatEnded = false;
-    res.render('combat', { players: players, isPlayer: players.includes(currentActor), actor: currentActor, combat: combatStatus() });
-});
+}
 
 //Process and save the damage the player deals to an enemy.
 //If the enemy dies and no enemies remain, combat ends in victory.
 //Returns a text string describing the result. (Damage taken or death)
 function parseDamage(data) {
     if(data === 'undefiend' || data === null || data.Result.DamageDealt === undefined) return undefined;
+    let target = (data.Target !== undefined) ? data.Target.Name : data.Result.SelectedTarget;
 
-    let actor = initiative.getActorData(data.Target.Name);
+    let actor = initiative.getActorData(target);
     actor.hp = data.Result.RemainingHP;
 
     var resultText;
@@ -71,35 +79,9 @@ function parseDamage(data) {
         resultText = `${actor.name} falls to the ground, defeated.`;
         actor.isActive = false;
 
-        if (getActiveEnemies().length < 1) {
+        if (getActiveEnemies().length < 1 || getActivePlayers().length < 1) {
             this.combatEnded = true;
-            this.playerVictory = true;
-        }
-    }
-
-    console.log(resultText);
-    return resultText;
-}
-
-//Process and save the damage an enemy deals to a player.
-//If the player dies and no party members remain, combat ends in defeat.
-//Returns a text string describing the result. (Damage taken or death)
-function parseEnemyDamage(data) {
-    if (data === 'undefiend' || data === null || data.Result.DamageDealt === undefined) return undefined;
-
-    let actor = initiative.getActorData(data.Result.SelectedTarget);
-    actor.hp = data.Result.RemainingHP;
-
-    var resultText;
-    if (actor.hp > 0) {
-        resultText = `${actor.name} took ${data.Result.DamageDealt} damage.`;
-    } else {
-        resultText = `${actor.name} falls to the ground, defeated.`;
-        actor.isActive = false;
-
-        if(getActivePlayers().length < 1) {
-            this.combatEnded = true;
-            this.playerVictory = false;
+            this.playerVictory = (getActiveEnemies().length < 1);
         }
     }
 
@@ -143,6 +125,17 @@ function combatStatus() {
         return {status: true, playerVictory: this.playerVictory};
     }
     return  {status: false};
+}
+
+//Checks if the response from chatGPT is null, and redirects to an error page.
+function verifyResponse(req, res, response, url) {
+    response = null;
+    if(response === null) {
+        //The below function works similar to a standard redirect, but treats it as a post request.
+        res.redirect(307,url);
+        return false;
+    }
+    return true;
 }
 
 //Sends the user to the victory page, and generates a combat outro.
@@ -202,12 +195,23 @@ router.post("/selectTarget/:target", async (req, res) => {
     let actor = initiative.getActorData(req.session.combatActor);
 
     //Get the dice that needs to be rolled from chatGPT.
-    let response = await openAI.generateRollRequest(prompts.generateBasicActionPrompt(actor.name, actor.actions[req.session.combatAction], req.params.target));
+
+    var roll;
+
+    if(actor.actions[req.session.combatAction].DamageDice === undefined) {
+    response = await openAI.generateRollRequest(prompts.generateBasicActionPrompt(actor.name, actor.actions[req.session.combatAction], req.params.target));
+
+    if(!verifyResponse(req,res,response,"/combat/selectTarget/" + req.params.target));
+    
     response = formatResponse(response);
 
-    //Parse the dice response, to allow the roll to happen.
     let data = JSON.parse(response);
-    let roll = (data.DamageDice).split('d');
+    roll = (data.DamageDice).split('d');
+    actor.actions[req.session.combatAction].DamageDice = roll;
+    }
+    else {
+        roll = actor.actions[req.session.combatAction].DamageDice;
+    }
 
     res.render('diceRoll', {
         Dice: roll[1],
@@ -218,28 +222,18 @@ router.post("/selectTarget/:target", async (req, res) => {
     });
 });
 
+function updateHistory(req, data) {
+        req.session.history.push(data.Result.ActionDescription);
+        req.session.history.push(parseDamage(data));
+        req.session.lastURL = req.url;
+}
+
 //Gets the values rolled by the player, and generates a response.
 router.post('/generatePlayerAction/:roll', async(req,res) => {
     let result = req.params.roll;
     result = result.toString().split('_');
 
-    const actor = req.session.combatActor;
-
     let current = initiative.currentTurn();
-    let friendly = isActorFriendly(current);
-    
-    //If for whatever reason, this post is reached while it is not a player's turn, return to the combat screen.
-    if (actor != current.name) {
-        res.render('combat', {
-            actions: [{ Action: `It is not ${actor}'s turn yet` }],
-            history: req.session.history,
-            players: players, 
-            isPlayer: friendly,
-            actor: current,
-            combat: combatStatus()
-        });
-        return;
-    }
 
     try {
         //Query the chatGPT API using the prompts generated from the player's selected action, target, and dice rolls.
@@ -249,21 +243,13 @@ router.post('/generatePlayerAction/:roll', async(req,res) => {
         let playerPrompt = prompts.playerTurnPrompt(current, prompts.assignAction(current.actions[req.session.combatAction], result[0], result[1]), initiative.getActorData(req.session.combatTarget), 300, 0.75);
 
         let text = await openAI.generateResponse(systemPrompt, playerPrompt);
+        if (!verifyResponse(req, res, text, '/combat')) {
+            return;
+        };
         text = formatResponse(text);
 
-        //Info to be displayed on the page later.
-        let data = [];
-        let damage = [];
-
-        //Parse the JSON received from chatGPT, into a readable format.
-        data.push(JSON.parse(text));
-
         // Store the received action, and update the history.
-        req.session.action = data;
-        for (var i = 0; i < data.length; i++) {
-            req.session.history.push(data[i].Result.ActionDescription);
-            req.session.history.push(parseDamage(data[i]));
-        }
+        updateHistory(req, JSON.parse(text));
 
         //End the current turn, and run the callback function of the next actor's turn, if one exists.
         await initiative.endTurn();
@@ -287,33 +273,21 @@ router.post('/generatePlayerAction/:roll', async(req,res) => {
 
 //Generates a response for the enemies actions.
 router.post('/generateAction/:actor', async (req, res) => {
-    const actor = req.params.actor;
-
     let current = initiative.currentTurn();
-    let friendly = isActorFriendly(current);
 
     try {
         //Query the chatGPT API using the prompts generated from the enemy who is acting, and the targetable players.
         let systemPrompt = prompts.enemySystemPrompt();
         let enemyPrompt = prompts.enemyTurnPrompt(current, getActivePlayers());
-        let text = await openAI.generateResponse(systemPrompt,enemyPrompt, 300, 0.75);
 
-        console.log("Non-Formatted: ",text);
+        let text = await openAI.generateResponse(systemPrompt,enemyPrompt, 300, 0.75);
+        if(!verifyResponse(req, res, text, '/combat')) {
+            return;
+        };
         text = formatResponse(text);
 
-        //Info to be displayed on the page later.
-        let data = [];
-        let damage = [];
-
         //Parse the JSON received from chatGPT, into a readable format.
-        data.push(JSON.parse(text));
-
-        // Store the received action, and update the history.
-        req.session.action = data;
-        for (var i = 0; i < data.length; i++) {
-            req.session.history.push(data[i].Result.ActionDescription);
-            req.session.history.push(parseEnemyDamage(data[i]));
-        }
+        updateHistory(req, JSON.parse(text));
 
         //End the current turn, and run the callback function of the next actor's turn, if one exists.
         //This repeats for every consecutive enemy action.
@@ -336,7 +310,7 @@ router.post('/generateAction/:actor', async (req, res) => {
 });
 
 //A formatting function for parsing JSON.
-//chatGPT likes to add comments which cannot be parsed, this function removes those comments.
+//chatGPT likes to add comments which cannot be parsed, this function removes them.
 function formatResponse(text) {
     var data = text;
 
